3718eed27612218be568ce3e0fd41bd18c61d130
Naza <sharon.doris54@gmail.com>
2024-05-03 18:10:11 -0700

    created the Home and Navbar components, added fonts, react-icons and mui material icons

---
+++ nazy-breakfast/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+    <div classname="home-container">
+        <Navbar/>
+    </div>
+    );
+};
+
+export default Home;
---
+++ nazy-breakfast/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Logo from "../Assets/Logo.svg";
+
+
+const Navbar = () => {
+    return <div>Navbar</div>;
+};
+
+export default Navbar;
